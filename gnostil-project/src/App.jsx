f8232413edf9ec0ae1aaabf5c4adf7b7fc3fe8d2
Dani <drbrejnak@gmail.com>
2025-02-19 04:24:00 -0500

    Initial Commit

---
+++ gnostil-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Modify from '../Maneuver_Properties/Modify'
+import Light from '../Maneuver_Properties/Light'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <Modify />
+      <Light />
+    </div>
+    </>
+  )
+}
+
+export default App
