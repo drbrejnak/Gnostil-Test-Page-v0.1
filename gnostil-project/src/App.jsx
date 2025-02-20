8ceade90ee4e4c9ae4f4dddea6b5cdec4f6b2325
Dani <drbrejnak@gmail.com>
2025-02-20 03:05:13 -0500

    Maneuver Properties Work

--- gnostil-project/src/App.jsx
+++ gnostil-project/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
 import Modify from '../Maneuver_Properties/Modify'
 import Light from '../Maneuver_Properties/Light'
+import Honorable from '../Maneuver_Properties/Honorable'
+import Combat from '../Maneuver_Properties/Combat'
+import Narrative from '../Maneuver_Properties/Narrative'
+import Infamous from '../Maneuver_Properties/Infamous'
+import Heavy from '../Maneuver_Properties/Heavy'
+import Attack from '../Maneuver_Properties/Attack'
+import Rings from '../Maneuver_Properties/Rings'
 
 function App() {
 
   return (
     <>
     <div className='container'>
-      <Modify />
-      <Light />
+      <Rings />
+      <Combat />
+      {/* <Narrative /> */}
+      <Honorable />
+      {/* <Infamous /> */}
+      {/* <Light /> */}
+      <Heavy />
+      {/* <Modify /> */}
+      <Attack />
     </div>
     </>
   )
